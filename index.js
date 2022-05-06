0bd50cb2e343135efda2fc3ceb64131e43c6157b
Galgoczki <norbigalgoczki@gmail.com>
2022-05-07 00:33:14 +0200

    make it better

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var corsOptions = {
       if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true)
       } else {
-        callback(new Error('Not allowed by CORS'))
+        callback(new Error('Not allowed by CORS',origin))
       }
     },
     credentials: true,
